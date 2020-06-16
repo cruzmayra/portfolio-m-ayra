61436f7810dfb48250fdeb06528cee19b7a99a7f
cruzmayra <velazquez.leonora@gmail.com>
2020-06-16 02:40:49 -0500

    Actualizar información personal

--- src/PortfolioContext.js
+++ src/PortfolioContext.js
@@ -9,7 +9,7 @@ const initialState = {
                 "Proyecto nacido de la necesidad de alinear la imagen del sitio web con los valores de la empresa y su nueva manera de hacer investigación de mercados."
             ],
             "link": "https://delarivagroup.com/",
-            "source-code": false,
+            "sourceCode": false,
             "codeLink": "",
             "previewImage": "dlRsiteCard.jpg",
             "heroImage": "dlRsiteHero.jpg"
@@ -23,7 +23,7 @@ const initialState = {
                 "Creada con la libreria React Native Web, obtiene las imágenes de gatos a través de la API REST de Unsplash. Surge de un ejercicio para practicar React Hooks y dar primeros pasos para crear aplicaciones móviles. Para no gestionar, de momento, un backend el login y los datos del usuario, como nombre y favoritos, se maneja con localStorage pero permite a diferentes 'usuarios' manejar independientemente su información."
             ],
             "link": "https://samaya-prueba-frontend-b01vh8zor.now.sh/",
-            "source-code": true,
+            "sourceCode": true,
             "codeLink": "https://github.com/cruzmayra/purrgram-project",
             "previewImage": "purrgramCard.jpg",
             "heroImage": "purrgramHero.jpg"
@@ -35,7 +35,7 @@ const initialState = {
                 "Proyecto individual que replica la página de Lab-Car con la utilización, en ese momento, por primera vez de un framework (Bootstrap) y la aplicación del responsive design."
             ],
             "link": "https://cruzmayra.github.io/lab-car-boilerplate/",
-            "source-code": true,
+            "sourceCode": true,
             "codeLink": "https://github.com/CruzMayra/lab-car-boilerplate",
             "previewImage": "labCarCard.jpg",
             "heroImage": "labCarHero.jpg"
@@ -48,7 +48,7 @@ const initialState = {
                 "Proyecto individual que hace uso de una GrahpQL API para realizar una sola petición por medio de jQuery. Aprovecha el manejo del DOM para crear de manera dinámina los modales con la información particular de cada pokémon."
             ],
             "link": "https://cruzmayra.github.io/pokedex/",
-            "source-code": true,
+            "sourceCode": true,
             "codeLink": "https://github.com/CruzMayra/pokedex",
             "previewImage": "pokedexCard.jpg",
             "heroImage": "pokedexHero.jpg"
@@ -58,12 +58,17 @@ const initialState = {
         "sobre-mi": {
             "title": "Sobre mí",
             "period": "",
-            "description": [],
+            "description": [
+                "Soy desarrolladora Frontend, radicada en CDMX.",
+                "Tengo dos años trabajando con herramientas como Javascript, ReactJS, React Hooks, React Native Web y WordPress. Recientemente di una charla en el Meetup de la Comunidad de WordPress en Puebla.",
+                "También tengo cuatro años de experiencia en labores administrativas, con Excel como mi principal herramienta.",
+                "Me gusta encontrar inspiración en el trabajo de los demás y aprender por mi cuenta."
+            ],
             "link": "",
-            "source-code": true,
+            "sourceCode": true,
             "codeLink": "",
-            "previewImage": "",
-            "heroImage": ""
+            "previewImage": "meCard.jpg",
+            "heroImage": "meHero.jpg"
         }   
     }
 }
