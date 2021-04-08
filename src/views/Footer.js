96af41265e9cb54ff9465ba645c68163ce9ada6f
Mayra Cruz <velazquez.leonora@gmail.com>
2021-04-08 17:01:14 -0500

    Cambiando estructura de información de contacto

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -5,39 +5,26 @@ export const Footer = () => {
     return (
         <footer>
             <section className="container">
-                <h3 className="title">Hey!</h3>
+                <h3 className="title">Mantente en contacto</h3>
                 <div className="column-container">
-                    <div className="first-column">
-                        <div>
-                            <p>Gracias por llegar al final de esta página. Si quieres saber más de mí o sobre qué estoy trabajando, consulta los enlaces de la derecha.</p>
-                        </div>
-                        <div>
-                            <p>
-                                Alternativamente, mira esta elegante hoja de papel.
-                                <br></br>
-                                <a href={'/Mayra-Cruz-Resume.pdf'}>Descarga mi CV</a>
-                            </p>
-                        </div>
-                    </div>
-                    <div className="second-column">
-                        <div>
-                            <p>Mantente en contacto</p>
-                            <a href="tomail:velazquez.leonora@gmail.com">velazquez.leonora@gmail.com</a>
-                        </div>
-                        <div>
-                            <p>Encuentrame también en:</p>
-                            <div className='footer-links-container'>
-                                <a className='footer-link' href='https://www.linkedin.com/in/cruz-mayra/'>
-                                    <FontAwesomeIcon className='link-icon' icon={['fab', 'linkedin-in']} />
-                                </a>
-                                <a className='footer-link' href='https://github.com/cruzmayra'>
-                                    <FontAwesomeIcon className='link-icon' icon={['fab', 'github']} />
-                                </a>
-                                {/* <a className='footer-link' href='https://codepen.io/m_ayra'>
-                                    <FontAwesomeIcon className='link-icon' icon={['fab', 'codepen']} />
-                                </a> */}
-                            </div>
-                        </div>
+                    
+                    <p>Gracias por llegar al final ✨. Si quieres saber más de mí, lo que estoy aprendiendo, en qué estoy trabajando o colaborando...</p>
+                    
+                    <p>Enviame un correo a: <a href="tomail:velazquez.leonora@gmail.com">velazquez.leonora@gmail.com</a></p>
+
+                    <p>Mira mi CV <a href={'/Mayra-Cruz-Resume.pdf'}>en este enlace.</a></p>
+                    
+                    <p>O bien, encuentrame acá:</p>
+                    <div className='footer-links-container'>
+                        <a className='footer-link' href='https://www.linkedin.com/in/cruz-mayra/'>
+                            <FontAwesomeIcon className='link-icon' icon={['fab', 'linkedin-in']} />
+                        </a>
+                        <a className='footer-link' href='https://github.com/cruzmayra'>
+                            <FontAwesomeIcon className='link-icon' icon={['fab', 'github']} />
+                        </a>
+                        {/* <a className='footer-link' href='https://codepen.io/m_ayra'>
+                            <FontAwesomeIcon className='link-icon' icon={['fab', 'codepen']} />
+                        </a> */}
                     </div>
                 </div>
             </section>
